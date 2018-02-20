c29138689fe92e4734bde9e468ef27736697c3cb
Russ Ferguson <russ.ferguson@kazzam.com>
2018-02-19 20:23:10 -0500

    nav body and header sections added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HeaderComponent from './header/Header';
+import NavComponent from './nav/Nav';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HeaderComponent />, document.getElementById('root'));
+ReactDOM.render(<NavComponent />, document.getElementById('nav'));
 registerServiceWorker();
